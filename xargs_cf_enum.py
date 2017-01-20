44f3d12a8297b0ee1f6f8d7172636099a18368f0
sharkbitme <sharkbitme@users.noreply.github.com>
2017-01-20 16:00:37 -0500

    Update xargs_cf_enum.py

--- xargs_cf_enum.py
+++ xargs_cf_enum.py
@@ -12,11 +12,8 @@ if len(sys.argv) != 2:
 	print("Target not specified! Usage: python cf_enum.py <target_ip_addr>")
 	sys.exit(0)
 host = sys.argv[1]
-#f = open(target_file, 'r')
-cfide_admin_path = '/CFIDE/administrator/index.cfm'
 
-#for host in f:
-#	host = host.rstrip('\n')
+cfide_admin_path = '/CFIDE/administrator/index.cfm'
 r = requests.get('http://' + host + cfide_admin_path, allow_redirects=True, 
 verify=False, timeout=5)
 	#print(r.url)
