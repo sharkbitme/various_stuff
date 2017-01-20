2c59604983e54ec04daec97a2ead2396241fbd0e
sharkbitme <sharkbitme@users.noreply.github.com>
2017-01-20 14:26:42 -0500

    Add files via upload

---
+++ cf_enum.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+This script attempts to enumerate cold fusion administrative interfaces
+probably poorly, ill bet this thing is slow, will improve it eventually
+usage: python cf_enum.py <file> where file is standard list of ip
+addresses or names. 
+
+"""
+import requests
+import sys
+
+target_file = sys.argv[1]
+f = open(target_file, 'r')
+cfide_admin_path = '/CFIDE/administrator/index.cfm'
+
+for host in f:
+	host = host.rstrip('\n')
+	r = requests.get('http://' + host + cfide_admin_path, allow_redirects=True, 
+	verify=False)
+	#print(r.url)
+	if r.status_code == 200:
+		print(r.url)
+	else:
+		continue
